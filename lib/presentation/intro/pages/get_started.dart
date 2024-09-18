d1970d9ea921d9dc63dd155e82489c2b61fc89cc
awoodywong <awoody988@gmail.com>
2024-09-19 01:00:46 +0800

    finished get_started page

---
+++ lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
+
+import '../../../core/configs/assets/app_images.dart';
+import '../../../core/configs/assets/app_vectors.dart';
+import '../../../core/configs/theme/app_colors.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill, image: AssetImage(AppImages.introBG))),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(
+                    AppVectors.logo,
+                  ),
+                ),
+                const Spacer(),
+                const Text(
+                  'Enjoy Listening To Music',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18),
+                ),
+                const SizedBox(height: 21),
+                const Text(
+                  'This is a flutter project that is built to mimic the Spotify app. It is built with the best practices of flutter development. And this context is just a dummy text.',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.grey,
+                      fontSize: 13),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                BasicAppButton(
+                    onPressed: () {
+                      //TODO: Navigate to the next page
+                    },
+                    title: 'Get Started'
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          )
+        ],
+      ),
+    );
+  }
+}
